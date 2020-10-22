f588fcac7ac65450e3ae63c199960ba9ba89f314
Isgeny <jack091196@gmail.com>
2020-10-22 20:34:27 +0300

    Изучение Subject'ов

---
+++ RxStudy/KeyTypes/ImplementingObserverObservableTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reactive.Disposables;
+using NUnit.Framework;
+
+namespace RxStudy.KeyTypes
+{
+    [TestFixture]
+    public class ImplementingObserverObservableTests
+    {
+        [Test]
+        public void ImplementingObserverObservableTest()
+        {
+            var sequenceOfNumbers = new SequenceOfNumbers();
+            var consoleObserver = new ConsoleObserver<int>();
+            sequenceOfNumbers.Subscribe(consoleObserver);
+        }
+    }
+
+    public class ConsoleObserver<T> : IObserver<T>
+    {
+        public void OnNext(T value)
+        {
+            Trace.WriteLine($"Received value {value}");
+        }
+
+        public void OnError(Exception error)
+        {
+            Trace.WriteLine($"Sequence faulted with {error}");
+        }
+
+        public void OnCompleted()
+        {
+            Trace.WriteLine("Sequence terminated");
+        }
+    }
+
+    public class SequenceOfNumbers : IObservable<int>
+    {
+        public IDisposable Subscribe(IObserver<int> observer)
+        {
+            observer.OnNext(1);
+            observer.OnNext(2);
+            observer.OnNext(3);
+            observer.OnCompleted();
+            return Disposable.Empty;
+        }
+    }
+}
---
+++ RxStudy/KeyTypes/SubjectTests.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reactive.Subjects;
+using System.Threading;
+using NUnit.Framework;
+
+namespace RxStudy.KeyTypes
+{
+    [TestFixture]
+    public class SubjectTests
+    {
+        [Test]
+        public void SubjectTest()
+        {
+            var subject = new Subject<string>();
+            subject.OnNext("a");
+            SubscribeConsole(subject);
+            subject.OnNext("b");
+            subject.OnNext("c");
+        }
+
+        [Test]
+        public void ReplaySubjectTest()
+        {
+            var subject = new ReplaySubject<string>();
+            subject.OnNext("a");
+            SubscribeConsole(subject);
+            subject.OnNext("b");
+            subject.OnNext("c");
+        }
+
+        [Test]
+        public void ReplaySubjectWithBufferSizeTest()
+        {
+            var subject = new ReplaySubject<string>(2);
+            subject.OnNext("a");
+            subject.OnNext("c");
+            subject.OnNext("d");
+            SubscribeConsole(subject);
+            subject.OnNext("e");
+        }
+
+        [Test]
+        public void ReplaySubjectWithWindowTest()
+        {
+            var subject = new ReplaySubject<string>(TimeSpan.FromMilliseconds(150));
+            subject.OnNext("a");
+            Thread.Sleep(TimeSpan.FromMilliseconds(100));
+            subject.OnNext("b");
+            Thread.Sleep(TimeSpan.FromMilliseconds(100));
+            subject.OnNext("c");
+            SubscribeConsole(subject);
+            subject.OnNext("d");
+        }
+
+        [Test]
+        public void BehaviorSubjectInitialTest()
+        {
+            var subject = new BehaviorSubject<string>("a");
+            SubscribeConsole(subject);
+        }
+
+        [Test]
+        public void BehaviorSubjectInitialNotPublishedTest()
+        {
+            var subject = new BehaviorSubject<string>("a");
+            subject.OnNext("b");
+            SubscribeConsole(subject);
+            subject.OnNext("c");
+            subject.OnNext("d");
+        }
+
+        [Test]
+        public void BehaviorSubjectNothingPublishedTest()
+        {
+            var subject = new BehaviorSubject<string>("a");
+            subject.OnNext("b");
+            subject.OnNext("c");
+            subject.OnCompleted();
+            SubscribeConsole(subject);
+        }
+
+        [Test]
+        public void AsyncSubjectNothingPublishedTest()
+        {
+            var subject = new AsyncSubject<string>();
+            subject.OnNext("a");
+            subject.OnNext("b");
+            SubscribeConsole(subject);
+            subject.OnNext("c");
+        }
+
+        [Test]
+        public void AsyncSubjectLastValuePublishedTest()
+        {
+            var subject = new AsyncSubject<string>();
+            subject.OnNext("a");
+            subject.OnNext("b");
+            SubscribeConsole(subject);
+            subject.OnNext("c");
+            subject.OnCompleted();
+        }
+
+        [Test]
+        public void SubjectNotPublishedAfterCompleteTest()
+        {
+            var subject = new Subject<string>();
+            subject.OnNext("a");
+            SubscribeConsole(subject);
+            subject.OnNext("b");
+            subject.OnCompleted();
+            subject.OnNext("c");
+        }
+
+        [Test]
+        public void SubjectNotPublishedAfterErrorTest()
+        {
+            var subject = new Subject<string>();
+            subject.OnNext("a");
+            SubscribeConsole(subject);
+            subject.OnNext("b");
+
+            try
+            {
+                subject.OnError(new Exception());
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
+
+            subject.OnNext("c");
+        }
+
+        private static void SubscribeConsole(IObservable<string> sequence)
+        {
+            sequence.Subscribe(x => Trace.WriteLine(x));
+        }
+    }
+}
