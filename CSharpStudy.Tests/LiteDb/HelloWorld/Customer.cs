885ed452f199aff15c5d5aba46c02b125c513a7c
Isgeny <jack091196@gmail.com>
2021-01-30 16:36:45 +0300

    Migrate litedb study

---
+++ CSharpStudy.Tests/LiteDb/HelloWorld/Customer.cs
@@ -0,0 +1,13 @@
+﻿namespace CSharpStudy.Tests.LiteDb.HelloWorld
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string[] Phones { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
---
+++ CSharpStudy.Tests/LiteDb/HelloWorld/HelloWorld.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using LiteDB;
+using NUnit.Framework;
+
+namespace CSharpStudy.Tests.LiteDb.HelloWorld
+{
+    [TestFixture]
+    public class HelloWorld
+    {
+        public class HelloLiteDb
+        {
+            [Test]
+            public void Test()
+            {
+                using var db = new LiteDatabase("hello.db");
+
+                var customers = db.GetCollection<Customer>();
+
+                var customer = new Customer
+                {
+                    Name = "John Doe",
+                    Phones = new[] {"8000-0000", "9000-0000"},
+                    IsActive = true
+                };
+                customers.Insert(customer);
+
+                customer.Name = "Jane Doe";
+                customers.Update(customer);
+
+                customers.EnsureIndex(x => x.Name);
+
+                var results = customers.Query()
+                    .Where(x => x.Name.StartsWith("J"))
+                    .OrderBy(x => x.Name)
+                    .Select(x => new {x.Name, NameUpper = x.Name.ToUpper()})
+                    .Limit(10)
+                    .ToList();
+
+                customers.EnsureIndex(x => x.Phones);
+
+                var r = customers.FindOne(x => x.Phones.Contains("8888-5555"));
+            }
+        }
+    }
+}
