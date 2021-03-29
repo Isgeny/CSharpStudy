10b2a9089de2517df8d6bcd1c2daa739b92c6924
Isgeny <jack091196@gmail.com>
2021-03-29 21:33:24 +0300

    Create .xlsx file and run Excel process

---
+++ CSharpStudy.Tests/ClosedXml/ClosedXmlTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using System.IO;
+using ClosedXML.Excel;
+using NUnit.Framework;
+
+namespace CSharpStudy.Tests.ClosedXml
+{
+    [TestFixture]
+    public class ClosedXmlTests
+    {
+        [Test]
+        public void Test()
+        {
+            var wb = new XLWorkbook();
+            var ws = wb.Worksheets.Add("Sheet1");
+
+            ws.Cell(1, 1).Value = "Hello World!";
+
+            var tempFilePath = $"{Path.GetTempPath()}Book1.xlsx";
+
+            wb.SaveAs(tempFilePath);
+
+            var processStartInfo = new ProcessStartInfo(tempFilePath)
+            {
+                UseShellExecute = true
+            };
+
+            Process.Start(processStartInfo);
+        }
+    }
+}
