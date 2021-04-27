452967e2832e4b2a51db551e8af2b609b7a6d911
Isgeny <jack091196@gmail.com>
2021-04-27 22:52:09 +0300

    Add antiroll and different view

---
+++ CSharpStudy.Render3D/Converters/InvertBoolConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace CSharpStudy.Render3D.Converters
+{
+    public class InvertBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value switch
+            {
+                bool x => !x,
+                _ => DependencyProperty.UnsetValue,
+            };
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value switch
+            {
+                bool x => !x,
+                _ => DependencyProperty.UnsetValue,
+            };
+        }
+    }
+}
