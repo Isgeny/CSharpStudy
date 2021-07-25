2437c1eafb2046e1fc4f4876b15b16fda2b61280
Isgeny <jack091196@gmail.com>
2021-07-25 15:18:29 +0300

    Enumerable vs single item array benchmark

---
+++ CSharpStudy.Benchmarks/EnumerableBench.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+
+namespace CSharpStudy.Benchmarks
+{
+    public class EnumerableBench
+    {
+        [Benchmark]
+        public int Enumerable() => Sum(EnumerableEx.Return(8));
+
+        [Benchmark]
+        public int Array() => Sum(new[] {8});
+
+        private static int Sum(IEnumerable<int> data) => data.Sum();
+    }
+
+    public static class EnumerableEx
+    {
+        public static IEnumerable<TResult> Return<TResult>(TResult value)
+        {
+            yield return value;
+        }
+    }
+}
---
+++ CSharpStudy.Benchmarks/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace CSharpStudy.Benchmarks
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            BenchmarkRunner.Run(typeof(Program).Assembly);
+        }
+    }
+}
