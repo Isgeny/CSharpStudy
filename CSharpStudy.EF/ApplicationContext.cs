3d58e7b89c1997d87f6ed84dd0d0fca792a0983a
Isgeny <jack091196@gmail.com>
2021-07-26 21:42:19 +0300

    Study EF tph

---
+++ CSharpStudy.EF/ApplicationContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CSharpStudy.EF
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=postgres;Username=postgres;Password=postgres");
+        }
+    }
+}
---
+++ CSharpStudy.EF/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace CSharpStudy.EF
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            using var db = new ApplicationContext();
+
+            var employee = new Employee
+            {
+                Firstname = "Travis",
+                LastName = "Howe"
+            };
+            db.Employees.Add(employee);
+
+            var customer = new Customer
+            {
+                Firstname = "Edward",
+                LastName = "Chester"
+            };
+            db.Customers.Add(customer);
+
+            db.SaveChanges();
+        }
+    }
+}
