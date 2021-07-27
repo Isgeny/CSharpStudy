3453bb3e9da0b229a7f4155f9085d6caf3fb0313
Isgeny <jack091196@gmail.com>
2021-07-27 21:59:18 +0300

    Study DbContext initialization

--- CSharpStudy.EF/ApplicationContext.cs
+++ CSharpStudy.EF/ApplicationContext.cs
@@ -4,13 +4,27 @@ namespace CSharpStudy.EF
 {
     public class ApplicationContext : DbContext
     {
+        private readonly string _connectionString;
+
+        public ApplicationContext(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+        }
+
         public DbSet<Customer> Customers { get; set; }
 
         public DbSet<Employee> Employees { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=postgres;Username=postgres;Password=postgres");
+            if (_connectionString is not null)
+            {
+                optionsBuilder.UseNpgsql(_connectionString);
+            }
         }
     }
 }
--- CSharpStudy.EF/Program.cs
+++ CSharpStudy.EF/Program.cs
@@ -1,26 +1,42 @@
-﻿namespace CSharpStudy.EF
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace CSharpStudy.EF
 {
     internal class Program
     {
         private static void Main()
         {
-            using var db = new ApplicationContext();
-
-            var employee = new Employee
+            const string connectionString = "Host=localhost;Port=5432;Database=postgres;Username=postgres;Password=postgres";
+            using (var db = new ApplicationContext(connectionString))
             {
-                Firstname = "Travis",
-                LastName = "Howe"
-            };
-            db.Employees.Add(employee);
+                db.Database.EnsureCreated();
+                var employee = new Employee
+                {
+                    Firstname = "Travis",
+                    LastName = "Howe"
+                };
+                db.Employees.Add(employee);
+                db.SaveChanges();
+            }
 
-            var customer = new Customer
+            var options = new DbContextOptionsBuilder<ApplicationContext>()
+                .EnableDetailedErrors()
+                .EnableSensitiveDataLogging()
+                .LogTo(Console.WriteLine)
+                .UseNpgsql(connectionString)
+                .Options;
+
+            using (var db = new ApplicationContext(options))
             {
-                Firstname = "Edward",
-                LastName = "Chester"
-            };
-            db.Customers.Add(customer);
-
-            db.SaveChanges();
+                var customer = new Customer
+                {
+                    Firstname = "Edward",
+                    LastName = "Chester"
+                };
+                db.Customers.Add(customer);
+                db.SaveChanges();
+            }
         }
     }
 }
